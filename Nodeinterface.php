155c0a02b3a7a374748f0111173cb7e6793dac01
ev <eduard.vihs@rambler.ru>
2022-02-13 22:05:02 +0300

    lesson 3

---
+++ Nodeinterface.php
@@ -0,0 +1,14 @@
+<?php
+
+interface NodeInterface
+{
+    public function __construct(sting $name);
+    public function __toSrting(): sring;
+    public function getName(): sring;
+    /**
+     * @RETURN Node[]
+     */
+    public function getChildren(): array;
+    public function addChildren(): self;
+}
+?>
